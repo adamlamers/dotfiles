42607a6b326b735e28df9978701ef6a4d4a40da4
Adam Lamers <adamlamers@gmail.com>
2026-02-11 21:19:46 -0500

    auto install vim-plug and move default shell configs into _default files

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,15 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
+local data_dir = vim.fn.stdpath('data')
+if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
+    vim.cmd('silent !curl -fLo ' ..
+    data_dir ..
+    '/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
+    vim.o.runtimepath = vim.o.runtimepath
+    vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
+end
+
 -- vim-plug
 local Plug = vim.fn["plug#"]
 local Plug_Begin = vim.fn["plug#begin"]
